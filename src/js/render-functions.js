d43de5e403c60b30f9a39dd5c5094a71617a663a
Allamoment <allamoment@gmail.com>
2025-11-22 22:35:58 +0900

    make a code

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,14 +1,17 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Елементи DOM
 const galleryEl = document.querySelector('.gallery');
 const loaderEl = document.querySelector('.loader');
 
+// Ініціалізація SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
+// Створення розмітки галереї
 export function createGallery(images) {
   const markup = images
     .map(
@@ -31,10 +34,10 @@ export function createGallery(images) {
           />
         </a>
         <div class="info">
-          <p><b>Likes</b>${likes}</p>
-          <p><b>Views</b>${views}</p>
-          <p><b>Comments</b>${comments}</p>
-          <p><b>Downloads</b>${downloads}</p>
+          <p><b>Likes</b> ${likes}</p>
+          <p><b>Views</b> ${views}</p>
+          <p><b>Comments</b> ${comments}</p>
+          <p><b>Downloads</b> ${downloads}</p>
         </div>
       </li>`
     )
@@ -44,14 +47,17 @@ export function createGallery(images) {
   lightbox.refresh();
 }
 
+// Очищення галереї
 export function clearGallery() {
   galleryEl.innerHTML = '';
 }
 
+// Показ лоадера
 export function showLoader() {
   loaderEl.classList.remove('is-hidden');
 }
 
+// Приховування лоадера
 export function hideLoader() {
   loaderEl.classList.add('is-hidden');
 }
